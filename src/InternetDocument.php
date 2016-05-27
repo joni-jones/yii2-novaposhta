01487e445fbd721038ec4e4fe3d4bce30b3b06f2
Sergii Gamaiunov <hello@webkadabra.com>
2016-05-27 13:31:07 +0300

    Add `getDocumentPrice` method for `InternetDocument` model

--- src/InternetDocument.php
+++ src/InternetDocument.php
@@ -4,10 +4,46 @@ namespace jones\novaposhta;
 /**
  * Class InternetDocument
  * @package jones\novaposhta
- *
+ * @author sergii gamaiunov <hello@webkadabra.com?
+ * 
  * This model contain methods to create and configure orders
  */
 final class InternetDocument extends Api
 {
+    const SCENARIO_DOCUMENT_PRICE = 'getDocumentPrice';
 
+    /** @var string */
+    public $CitySender;
+    /** @var string */
+    public $CityRecipient;
+    /** @var string (DoorsDoors|DoorsWarehouse|WarehouseWarehouse|WarehouseDoors) */
+    public $ServiceType;
+    /** @var integer|float  */
+    public $Weight;
+    /** @var float */
+    public $Cost;
+    /** @var string */
+    public $DateTime;
+    /** @var float volume of parcel in qubic centimeters (width * height * length / 1000000) */
+    public $VolumeGeneral;
+
+    public $SenderAddress;
+    public $SeatsAmount;
+    public $CargoType;
+
+    /**
+     * Get price of delivery between two cities
+     * @return mixed
+     */
+    function getDocumentPrice() {
+        $query = [
+            'CitySender' => $this->CitySender,
+            'CityRecipient' => $this->CityRecipient,
+            'ServiceType' => $this->ServiceType,
+            'Weight' => $this->Weight,
+            'Cost' => $this->Cost,
+            'VolumeGeneral' => $this->VolumeGeneral
+        ];
+        return $this->call('getDocumentPrice', $query);
+    }
 }
