60bf37cee8640b0b3f017c1dfc302a9402a84e71
Sergii Gamaiunov <hello@webkadabra.com>
2016-07-29 02:36:21 +0300

    Add API methods to create & update `ContactPerson`

--- src/ContactPerson.php
+++ src/ContactPerson.php
@@ -1,13 +1,114 @@
 <?php
 namespace jones\novaposhta;
+use yii\helpers\ArrayHelper;
 
 /**
  * Class ContactPerson
  * @package jones\novaposhta
+ * @author sergii gamaiunov <hello@webkadabra.com>
  *
  * This model contain methods to work with contact person data
  */
 final class ContactPerson extends Api
 {
+    /**
+     * @var string
+     */
+    public $CounterpartyRef;
 
+    /**
+     * @var string
+     */
+    public $FirstName;
+
+    /**
+     * @var string
+     */
+    public $LastName;
+
+    /**
+     * @var string
+     */
+    public $MiddleName;
+
+    /**
+     * @var string
+     */
+    public $Phone;
+
+    /**
+     * @var string
+     */
+    public $Email;
+
+    /**
+     * @inheritdoc
+     * @codeCoverageIgnore
+     */
+    public function rules()
+    {
+        $rules = parent::rules();
+        return ArrayHelper::merge($rules, [
+            [['CounterpartyRef', 'FirstName', 'LastName', 'MiddleName', 'Email', 'Phone'], 'string'],
+            ['Email', 'email'],
+            [
+                ['CounterpartyRef', 'FirstName', 'LastName', 'Phone'],
+                'required', 'on' => self::SCENARIO_SAVE
+            ],
+            [
+                ['FirstName', 'LastName', 'Phone'],
+                'required', 'on' => self::SCENARIO_UPDATE
+            ],
+        ]);
+    }
+
+    /**
+     * @inheritdoc
+     * @codeCoverageIgnore
+     */
+    public function scenarios()
+    {
+        $scenarios = parent::scenarios();
+        $scenarios[self::SCENARIO_SAVE] = [
+            'CounterpartyRef', 'FirstName', 'LastName', 'MiddleName', 'Email', 'Phone'
+        ];
+        $scenarios[self::SCENARIO_UPDATE] = [
+            'FirstName', 'LastName', 'MiddleName', 'Email', 'Phone'
+        ];
+        return $scenarios;
+    }
+
+    public function getCounterpartyContactPerson() {
+        return $this->call('getCounterpartyContactPerson');
+    }
+
+    /**
+     * Create new contractor
+     * @return array|bool
+     */
+    public function save()
+    {
+        return $this->saveContactPerson(self::SCENARIO_SAVE);
+    }
+
+    /**
+     * Update contractor details
+     * @return array|bool
+     */
+    public function update()
+    {
+        return $this->saveContactPerson(self::SCENARIO_UPDATE);
+    }
+
+    /**
+     * Save contractor details
+     * @param string $method
+     * @return array|bool
+     */
+    protected function saveContactPerson($method)
+    {
+        $this->setScenario($method);
+        $this->enableValidation();
+        return $this->call($method);
+    }
 }
