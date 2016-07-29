3521da7aa03976d9e638faa51ac3f152a2f2789b
Sergii Gamaiunov <hello@webkadabra.com>
2016-07-29 04:01:15 +0300

    Complete `saveDocument` method

--- src/InternetDocument.php
+++ src/InternetDocument.php
@@ -31,6 +31,17 @@ final class InternetDocument extends Api
     public $SeatsAmount;
     public $CargoType;
 
+    public $PayerType = 'Sender';
+    public $PaymentMethod = 'Cash';
+    public $Sender;
+    public $ContactSender;
+    public $SendersPhone;
+    public $RecipientAddress;
+    public $Recipient;
+    public $RecipientsPhone;
+    public $ContactRecipient;
+    public $Description;
+
     /**
      * Get price of delivery between two cities
      * @return mixed
@@ -45,5 +56,34 @@ final class InternetDocument extends Api
             'VolumeGeneral' => $this->VolumeGeneral
         ];
         return $this->call('getDocumentPrice', $query);
+    }
+
+    /**
+     * Get price of delivery between two cities
+     * @return mixed
+     */
+    function saveDocument() {
+        $query = [
+            'Sender' => $this->Sender,
+            'PayerType' => $this->PayerType,
+            'PaymentMethod' => $this->PaymentMethod,
+            'CargoType' => $this->CargoType,
+            'SenderAddress' => $this->SenderAddress,
+            'ContactSender' => $this->ContactSender,
+            'SendersPhone' => $this->SendersPhone,
+            'RecipientAddress' => $this->RecipientAddress,
+            'Recipient' => $this->Recipient,
+            'ContactRecipient' => $this->ContactRecipient,
+            'RecipientsPhone' => $this->RecipientsPhone,
+            'Description' => $this->Description,
+
+            'CitySender' => $this->CitySender,
+            'CityRecipient' => $this->CityRecipient,
+            'ServiceType' => $this->ServiceType,
+            'Weight' => $this->Weight,
+            'Cost' => $this->Cost,
+            'VolumeGeneral' => $this->VolumeGeneral,
+        ];
+        return $this->call('save', $query);
     }
 }
